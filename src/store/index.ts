da56ba3d016eee69a5f04e9b55d8f7a38986954d
t-kiyono <t-kiyono@be-geek.net>
2019-08-07 15:25:19 +0900

    fix redux-devtools-extension

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,20 +1,18 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import counterReducer from '../counter/reducer';
 import rootSaga from './saga';
 import { AppState } from './state';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const middleWare = applyMiddleware(sagaMiddleware);
-const devMiddleWare = composeWithDevTools(middleWare);
+const composeEnhancer: typeof compose = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   combineReducers<AppState>({
     counter: counterReducer
   }),
-  process.env.NODE_ENV === 'development' ? devMiddleWare : middleWare
+  composeEnhancer(applyMiddleware(sagaMiddleware))
 );
 
 sagaMiddleware.run(rootSaga);
